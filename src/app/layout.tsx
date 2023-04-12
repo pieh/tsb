f7ab14c4dbfb3ef63ea69c5d6f6cee638d4b809e
Davide Crestini <davide.crestini94@gmail.com>
2023-04-12 17:25:51 +0700

    chore: improved SEO setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,42 +10,57 @@ export const metadata = {
   },
   description: "Travel Blog of The Scrapbookers",
   generator: "Next.js",
-  keywords: ["Travel", "South East Asia", "JavaScript"],
+  keywords: ["Travel", "South East Asia", "Blog", "Food and travel"],
   authors: [
     { name: "Davide Crestini", url: "https://dcrestini.me" },
     { name: "Beatrice Cox", url: "https://beatricecox.com" },
   ],
   creator: "Davide Crestini",
   publisher: "Beatrice Cox",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/icons/apple-touch-icon.png",
+    other: [
+      {
+        url: "/icons/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/icons/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+    ],
+  },
   formatDetection: {
     email: false,
     address: false,
     telephone: false,
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'The Scrapbookers',
-    description: 'Travel Blog of The Scrapbookers',
+    card: "summary_large_image",
+    title: "The Scrapbookers",
+    description:
+      "Travel couple, currently travelling through South East Asia. We want to bring a new perspective about the places we visit, as well as some interesting stories.",
     // siteId: '1467726470533754880',
     // creator: '@nextjs',
     // creatorId: '1467726470533754880',
     // images: ['https://nextjs.org/og.png'],
   },
-  // images: [
-  //   {
-  //     url: 'https://nextjs.org/og.png',
-  //     width: 800,
-  //     height: 600,
-  //   },
-  //   {
-  //     url: 'https://nextjs.org/og-alt.png',
-  //     width: 1800,
-  //     height: 1600,
-  //     alt: 'My custom alt',
-  //   },
-  // ],
-  locale: 'en-GB',
-  type: 'website',
+  openGraph: {
+    type: "website",
+    url: "https://thescrapbookers.blog",
+    title: "The Scrapbookers",
+    description:
+      "Travel couple, currently travelling through South East Asia. We want to bring a new perspective about the places we visit, as well as some interesting stories.",
+    siteName: "The Scrapbookers",
+    images: [
+      {
+        url: "https://thescrapbookers.blog/images/home-hero.jpg",
+      },
+    ],
+  },
 };
 
 const WEBSITE_IS_ACTIVE = process.env.WEBSITE_IS_ACTIVE === "true" || false;
