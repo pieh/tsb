3c954f487729fa89b864687046af25048c5255ea
Davide Crestini <davide.crestini94@gmail.com>
2023-04-12 16:59:02 +0700

    feat: added link for small notes

--- src/components/SmallNotesSection/SmallNotePost.tsx
+++ src/components/SmallNotesSection/SmallNotePost.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { poppins } from "utils/fonts";
 import type { Post as IPost } from "types/global";
 
-export const SmallNotePost: React.FC<IPost> = ({ title, category }) => {
+export const SmallNotePost: React.FC<IPost> = ({ title, category, slug }) => {
   return (
     <div className="flex flex-col w-full">
-      <h3 className="text-3xl mb-2 lg:mb-4">{title}</h3>
+      <Link href={`/post/${slug}`}>
+        <h3 className="text-3xl mb-2 lg:mb-4">{title}</h3>
+      </Link>
       <div
         className="flex items-center uppercase tracking-widest text-xs text-gray-400"
         style={poppins.style}
